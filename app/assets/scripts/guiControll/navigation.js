16e5bc327cb16e56a5518619c4111093e3558094
Thomas Frey <joe@mitlinxlernen.ch>
2017-11-21 21:12:30 +0100

    code in navigation.js modularized

--- app/assets/scripts/guiControll/navigation.js
+++ app/assets/scripts/guiControll/navigation.js
@@ -3,26 +3,28 @@
 var Navigation = (function(){
     console.log('inside Navigation IIFE');
 
+    var hamburger,
+        mainNavi;
+
+    function showHideNavigation() {
+        if(mainNavi.style.display === 'block'){
+            mainNavi.style.display = 'none';
+        } else {
+            mainNavi.style.display = 'block';
+        }
+    }
 
     function init(){
-        var hamburger = document.getElementById('hamburger');
+        hamburger = document.getElementById('hamburger');
+        mainNavi = document.getElementById('mainNavi');
 
-        hamburger.addEventListener('click', function(){
-            var navi = document.getElementById('mainNavi');
-
-            if(navi.style.display === 'block'){
-                navi.style.display = 'none';
-            } else {
-                navi.style.display = 'block';
-            }
-        });
+        hamburger.addEventListener('click', showHideNavigation);
     }
 
     //public api
     return {
         init: init
     };
-
 })();
 
 
