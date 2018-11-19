15a68508040a59e14b5bbfa91cdb2c15f87d37ab
Thomas Frey <joe@mitlinxlernen.ch>
2018-11-19 21:41:57 +0100

    Kanukurse Termine Tabelle und Slider

--- app/assets/js/guiControll/navigation.js
+++ app/assets/js/guiControll/navigation.js
@@ -3,30 +3,42 @@
 var Navigation = (function(){
     console.log('inside Navigation IIFE');
 
+    const SHOW = 'js-show'
+
     var hamburger,
-        mainNavi;
+        mainNavi,
+        basisKursDrawerButton;
 
     function showHideNavigation() {
-        if(/*mainNavi.style.display === 'block'*/mainNavi.classList.contains('hide')){
-            //mainNavi.style.display = 'none';
+        if(mainNavi.classList.contains('hide')){
             mainNavi.classList.remove('hide')
         } else {
-            //mainNavi.style.display = 'block';
             mainNavi.classList.add('hide');
         }
     }
 
+
+    function toggleTextContainerDrawer() {
+        const basisKursDrawer = basisKursDrawerButton.parentElement.parentElement.lastElementChild;
+        const basisKursButtonIcon = basisKursDrawerButton.lastElementChild;
+
+        if(basisKursDrawer.classList.contains(SHOW)){
+            basisKursDrawer.classList.remove(SHOW);
+            basisKursButtonIcon.classList.remove(SHOW);
+        } else {
+            basisKursDrawer.classList.add(SHOW);
+            basisKursButtonIcon.classList.add(SHOW);
+        }
+    }
+
+
     function init(){
         hamburger = document.getElementById('hamburger');
         mainNavi = document.getElementById('mainNavi');
-
         hamburger.addEventListener('click', showHideNavigation);
 
-      /*  window.onresize = function() {
-            if(window.innerWidth >= 740) {
-                mainNavi.style.display = '';
-            }
-        }; */
+        basisKursDrawerButton = document.querySelector('#open-close-drawer-button-basiskurse');
+        basisKursDrawerButton.addEventListener('click', toggleTextContainerDrawer)
     }
 
     //public api
