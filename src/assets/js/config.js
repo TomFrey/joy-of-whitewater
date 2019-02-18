362f9e91b237102b9f57ee408583abf958724400
Thomas Frey <joe@mitlinxlernen.ch>
2019-02-18 07:03:57 +0100

    Neue Titelbilder

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,17 +2,23 @@
 var Configuration = (function () {
 	const imageCarousel = {
 		theJoyOfWhiteWaterImages: [
+			{ name: 'joyOfWhitewater_5', title: 'Big Wave in Idaho', alt: 'The Joy Of Whitewater' },
 			{ name: 'joyOfWhitewater_4', title: 'Big Wave in Idaho', alt: 'The Joy Of Whitewater' },
 			{ name: 'joyOfWhitewater_3', title: 'Entspannung am trocknen', alt: 'The Joy Of Whitewater' },
 			{ name: 'joyOfWhitewater_2', title: 'Paddeln in Costa Rica', alt: 'The Joy Of Whitewater' },
 			{ name: 'joyOfWhitewater_1', title: 'Paddeln in Costa Rica', alt: 'The Joy Of Whitewater' }
 		],
 		kanukurseImages: [
+			{ name: 'kanukurse_5', title: 'Kanukurs', alt: 'Kanukurs' },
+			{ name: 'kanukurse_4', title: 'Kanukurs', alt: 'Kanukurs' },
 			{ name: 'kanukurse_3', title: 'Kanukurs', alt: 'Kanukurs' },
 			{ name: 'kanukurse_2', title: 'Kanukurs', alt: 'Kanukurs' },
 			{ name: 'kanukurse_1', title: 'Kanukurs', alt: 'Kanukurs' }
 		],
 		paddelreisenImages: [
+			{ name: 'paddelreisen_8', title: 'Paddelreisen', alt: 'Paddelreisen' },
+			{ name: 'paddelreisen_7', title: 'Paddelreisen', alt: 'Paddelreisen' },
+			{ name: 'paddelreisen_6', title: 'Paddelreisen', alt: 'Paddelreisen' },
 			{ name: 'paddelreisen_5', title: 'Paddelreisen', alt: 'Paddelreisen' },
 			{ name: 'paddelreisen_4', title: 'Paddelreisen', alt: 'Paddelreisen' },
 			{ name: 'paddelreisen_3', title: 'Paddelreisen', alt: 'Paddelreisen' },
